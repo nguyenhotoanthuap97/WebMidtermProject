0ef7c235a45963aa38194c545744bfa9373e1b03
TienLaptop\Tien <tienandehit@gmail.com>
2018-04-20 19:35:30 +0700

    1.Them vai hinh 2.Login them username vo cho khac

---
+++ js/login.js
@@ -0,0 +1,13 @@
+﻿function ChangeToUser(username)
+{
+    $("#tool-bar").prepend('<li id ="acount-tag"><a href="#"><i class="fa fa-user"></i>Tài khoản: '+username+' </a></li>');
+    $("#login-tag").remove();
+    $("#tool-bar").append('<li id ="logout-tag"><a href="#" onclick = "ChangeToGuess()"><i class="fa fa-user"></i> Đăng xuất</a></li>');
+}
+
+function ChangeToGuess()
+{
+    $("#acount-tag").remove();
+    $("#logout-tag").remove();
+    $("#tool-bar").append('<li id = "login-tag"><a href="#" data-toggle="modal" data-target="#Login"><i class="fa fa-user"></i> Đăng nhập</a></li>');        
+}
