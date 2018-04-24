7313be31d2239341afd6c2d7b725b3903ee6a300
TienLaptop\Tien <tienandehit@gmail.com>
2018-04-24 17:45:45 +0700

    them lai price slide

--- js/main.js
+++ js/main.js
@@ -96,6 +96,18 @@ jQuery(document).ready(function ($) {
 
     //hide other address
     $(".shipping_address").hide(); 
+
+    $("#slider-range").slider({
+        range: true,
+        min: 0,
+        max: 2000,
+        values: [100, 1500],
+        slide: function (event, ui) {
+            $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+        }
+    });
+    $("#amount").val("$" + $("#slider-range").slider("values", 0) +
+        " - $" + $("#slider-range").slider("values", 1));
 });
 
 function otherAddress()
