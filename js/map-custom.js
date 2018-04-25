ee8f6ce2aa16937078c62a61c00a35fcdee57474
Phan Trọng Thuyên <admin@admins-MacBook-Air.local>
2018-04-25 20:14:48 +0700

    1512557_cap nhat lien he

--- js/map-custom.js
+++ js/map-custom.js
@@ -15,8 +15,8 @@
                 img_pin = 'images/icons/location.png';
             }
             if (data_map_x == null || data_map_y == null) {
-                data_map_x = 40.007749;
-                data_map_y = -93.266572;
+                data_map_x = 10.791527;
+                data_map_y = 106.685627;
             }
             if (scrollwhell == null) {
                 scrollwhell = 0;
