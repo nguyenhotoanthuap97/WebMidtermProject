fd19345618f63fdff05d43713d8781d4cbfd353c
DESKTOP-B35FHBP\Thoai <thoaihuynhtrong@gmail.com>
2018-04-28 13:42:26 +0700

    1512551_Admin Thong Ke

--- js/main.js
+++ js/main.js
@@ -726,7 +726,50 @@ jQuery(document).ready(function ($) {
         $('.cart-subtotal').find('.amount').text(addDotIntoMoney(total_money) + ' đ');
         $('.order-total').find('.amount').val(total_money);
         $('.order-total').find('.amount').text(addDotIntoMoney(total_money) + ' đ');
+    });
+
+    
+    $('.dashboard-products').bind('click' , function() {
+        $('.statistics-provinces').hide();
+        $('.statistics-products').show();
+        $('.statistics-classify').hide();
+        $('.statistics-brands').hide();
+
+        
+        $('.dashboard').removeClass('active');
+        $('.dashboard-products').addClass('active');
+        $('.dashboard-classify').removeClass('active');
+        $('.dashboard-brands').removeClass('active');
     })
+
+    $('.dashboard-classify').bind('click' , function() {
+        $('.statistics-provinces').hide();
+        $('.statistics-products').hide();
+        $('.statistics-classify').show();
+        $('.statistics-brands').hide();
+        
+
+        $('.dashboard').removeClass('active');
+        $('.dashboard-products').removeClass('active');
+        $('.dashboard-classify').addClass('active');
+        $('.dashboard-brands').removeClass('active');
+    })
+
+    $('.dashboard-brands').bind('click' , function() {
+        $('.statistics-provinces').hide();
+        $('.statistics-products').hide();
+        $('.statistics-classify').hide();
+        $('.statistics-brands').show();
+
+        $('.dashboard').removeClass('active');
+        $('.dashboard-products').removeClass('active');
+        $('.dashboard-classify').removeClass('active');
+        $('.dashboard-brands').addClass('active');
+    })
+
+    statisticsProducts(products);
+    statisticsBrands(products);
+    statisticsClassify(products);
 });
 
 function loadShop(product_list, id) {
@@ -773,6 +816,8 @@ function processSelectCountries(id) {
     const country_list = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua &amp; Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia &amp; Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Chad", "Chile", "China", "Colombia", "Congo", "Cook Islands", "Costa Rica", "Cote D Ivoire", "Croatia", "Cruise Ship", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kuwait", "Kyrgyz Republic", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Mauritania", "Mauritius", "Mexico", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Namibia", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pakistan", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Pierre &amp; Miquelon", "Samoa", "San Marino", "Satellite", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "South Africa", "South Korea", "Spain", "Sri Lanka", "St Kitts &amp; Nevis", "St Lucia", "St Vincent", "St. Lucia", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor L'Este", "Togo", "Tonga", "Trinidad &amp; Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks &amp; Caicos", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Venezuela", "Vietnam", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe"];
 
     let select = $(id);
+    if (select.length == 0)
+        return;
     for (country of country_list) {
         var opt = document.createElement('option');
         opt.value = country;
@@ -907,3 +952,110 @@ function addDotIntoMoney(money) {
     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
     return parts.join(".");
 }
+
+
+function statisticsProducts(products) {
+    let body = $('.statistics-products').find('tbody');
+    let tr;
+    let td;
+    let text;
+    for (product of products) {
+        tr = document.createElement('tr');
+
+        td = document.createElement('td');
+        text = document.createTextNode(product.name);
+        td.appendChild(text);
+        tr.appendChild(td);
+
+        td = document.createElement('td');
+        text = document.createTextNode('$' + addDotIntoMoney(product.price));
+        td.appendChild(text);
+        tr.appendChild(td);
+        
+        td = document.createElement('td');
+        text = document.createTextNode(Math.floor((Math.random() * 100) + 1));
+        td.appendChild(text);
+        tr.appendChild(td);
+
+        body.append(tr);
+    }
+}
+
+function statisticsBrands(products) {
+    let Brand = {};
+    let Price = {};
+    let arr = [];
+    let body = $('.statistics-brands').find('tbody'); 
+    let td,tr,text;
+
+    //Filter products same to Object
+    products.forEach(element => {
+        Brand[element.brand] = (Brand[element.brand] || 0) + 1;
+        Price[element.brand] = (Price[element.brand] || 0) + element.price;
+    });
+
+    //Push Object To Array
+    for (key in Brand) {
+        arr.push({brand: key, count: Brand[key], price: Price[key]});
+    }
+    
+    for (item of arr) {
+        tr = document.createElement('tr');
+        td = document.createElement('td');
+        text = document.createTextNode(item.brand);
+        td.appendChild(text);
+        tr.appendChild(td);
+
+        td = document.createElement('td');
+        text = document.createTextNode(item.count);
+        td.appendChild(text);
+        tr.appendChild(td);
+        
+        td = document.createElement('td');
+        text = document.createTextNode('$' + addDotIntoMoney(item.price))
+        td.appendChild(text);
+        tr.appendChild(td);
+
+        body.append(tr);
+    }
+}
+
+function statisticsClassify(products) {
+    let Classify = {};
+    let Price = {};
+    let arr = [];
+    let body = $('.statistics-classify').find('tbody'); 
+    let td,tr,text;
+
+    //Filter products same to Object
+    products.forEach(element => {
+        Classify[element.class] = (Classify[element.class] || 0) + 1;
+        Price[element.class] = (Price[element.class] || 0) + element.price;
+    });
+
+    //Push Object To Array
+    for (key in Classify) {
+        arr.push({classify: key, count: Classify[key], price: Price[key]});
+    }
+    
+    for (item of arr) {
+        tr = document.createElement('tr');
+        td = document.createElement('td');
+        text = document.createTextNode(item.classify);
+        td.appendChild(text);
+        tr.appendChild(td);
+
+        td = document.createElement('td');
+        text = document.createTextNode(item.count);
+        td.appendChild(text);
+        tr.appendChild(td);
+        
+        td = document.createElement('td');
+        text = document.createTextNode('$' + addDotIntoMoney(item.price))
+        td.appendChild(text);
+        tr.appendChild(td);
+
+        body.append(tr);
+    }
+}
+
